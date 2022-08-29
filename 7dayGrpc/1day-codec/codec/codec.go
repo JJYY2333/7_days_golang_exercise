72f7fe0bc816fcca3ee01d0c23c642c16a229948
JJYY2333 <824778124@qq.com>
2022-08-30 01:46:40 +0800

    # first day ok

--- 7dayGrpc/1day-codec/codec/codec.go
+++ 7dayGrpc/1day-codec/codec/codec.go
@@ -12,7 +12,23 @@ type Header struct {
 
 type Codec interface {
 	io.Closer
-	ReadHeader(*Header) error
-	ReadBody(interface{}) error
+	ReadHeader(*Header) 	error
+	ReadBody(interface{}) 	error
 	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const(
+	GobType Type = "application/gob"
+	JsonType Type = "application/json" // not implement
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init(){
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
 }
